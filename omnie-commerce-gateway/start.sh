30d6ec18787beb9b7cc2fa09e68755aa264cb5a9
Alexander Balyshyn <balyszyn@gmail.com>
2018-02-23 01:38:41 +0200

    fix docker-compose url`s compatibility Signed-off-by: Alexander Balyshyn <balyszyn@gmail.com>

--- omnie-commerce-gateway/start.sh
+++ omnie-commerce-gateway/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Try to connect to the discovery service"
-while [[ "$(curl -w ''%{http_code}'' http://omnie-discovery:9998/status)" != "200" ]]; do sleep 50s; done
+while [[ "$(curl -w ''%{http_code}'' http://omnie_discovery:9998/status)" != "200" ]] ; do sleep 50s; done
 echo "Connected to the discovery service"
 java $JAVA_OPTS -jar /app.jar $APP_OPTS
--- omnie-commerce-security/start.sh
+++ omnie-commerce-security/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Try to connect to the gateway service"
-while [["$(curl -w ''%{http_code}'' http://omnie-gateway:9999/status)" !=  "200" ]]; do sleep 50s; done
+while [["$(curl -w ''%{http_code}'' http://omnie_gateway:9999/status)" !=  "200" ]]; do sleep 50s; done
 echo "Connected to the gateway service"
 java $JAVA_OPTS -jar /app.jar $APP_OPTS
