0d02cd6580b39b870eb443e805b60a4ce2c42e60
Vitalii Martynovskyi <vitaliy.martynovskyy@ukr.net>
2018-02-27 23:19:25 +0200

    delete chain setters

--- omnie-commerce-owner/src/main/java/com/omniesoft/commerce/owner/controller/management/payload/PermissionByOrganizationPayload.java
+++ omnie-commerce-owner/src/main/java/com/omniesoft/commerce/owner/controller/management/payload/PermissionByOrganizationPayload.java
@@ -2,14 +2,12 @@ package com.omniesoft.commerce.owner.controller.management.payload;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.experimental.Accessors;
 
 import java.util.Set;
 import java.util.UUID;
 
 @Data
-@Accessors(chain = true)
+//@Accessors(chain = true)
 @NoArgsConstructor
 public class PermissionByOrganizationPayload {
     private UUID organizationId;
