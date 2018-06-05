9857f1207c6d657136cba7a0a1d963d984b40e7c
Vitalii Martynovskyi <vitaliy.martynovskyy@ukr.net>
2018-06-05 09:47:25 +0300

    fix listener

--- omnie-commerce-notification/src/main/java/com/omniesoft/commerce/notification/event/admin/events/OrderEventListener.java
+++ omnie-commerce-notification/src/main/java/com/omniesoft/commerce/notification/event/admin/events/OrderEventListener.java
@@ -2,7 +2,8 @@ package com.omniesoft.commerce.notification.event.admin.events;
 
 import com.omniesoft.commerce.common.Constants;
 import com.omniesoft.commerce.notification.ISearchAdminService;
-import com.omniesoft.commerce.notification.util.event.UserNotifyEvent;
+import com.omniesoft.commerce.notification.event.UserEventScope;
+import com.omniesoft.commerce.notification.util.event.admin.events.OrderNotifEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.EventListener;
@@ -12,15 +13,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class OrderEventListener implements ApplicationListener<UserNotifyEvent> {
+public class OrderEventListener implements ApplicationListener<OrderNotifEvent> {
     private final SimpMessageSendingOperations ws;
     private final ISearchAdminService findAdmin;
 
     @Override
     @EventListener
-    public void onApplicationEvent(UserNotifyEvent event) {
-        String owner = findAdmin.getOwner(event.getScope().getOrganizationReceiver());
-        ws.convertAndSendToUser(owner, Constants.WS.Owner.NOTIFICATION, event.getSource());
+    public void onApplicationEvent(OrderNotifEvent event) {
+
+        if (event.getScope() instanceof UserEventScope) {
+            UserEventScope scope = (UserEventScope) event.getScope();
+            String owner = findAdmin.getOwner(scope.getOrganizationReceiver());
+            ws.convertAndSendToUser(owner, Constants.WS.Owner.NOTIFICATION, event.getSource());
+        } else {
+            AdminEventScope scope = (AdminEventScope) event.getScope();
+            ws.convertAndSendToUser(scope.getUserReceiver(), Constants.WS.User.NOTIFICATION, event.getSource());
+        }
 
     }
 }
